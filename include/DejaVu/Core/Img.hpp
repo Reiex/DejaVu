0a26ec01e46ec1a0c6873c37d1b63d36b3c33c17
Reiex <reiex.marius38@gmail.com>
2020-11-23 00:18:00 +0100

    Début API

--- include/DejaVu/Core/Img.hpp
+++ include/DejaVu/Core/Img.hpp
@@ -4,5 +4,55 @@
 
 namespace djv
 {
+	enum class ColorChannel
+	{
+		RED = 1,
+		GREEN = 2,
+		BLUE = 4,
+		ALPHA = 8
+	};
 
+	ColorChannel operator|(ColorChannel a, ColorChannel b);
+	ColorChannel operator&(ColorChannel a, ColorChannel b);
+
+	struct Pixel
+	{
+		float& r;
+		float& g;
+		float& b;
+		float& a;
+	};
+
+	class Img
+	{
+		public:
+
+			Img() = delete;
+			Img(const std::string& filename);
+			Img(uint64_t width, uint64_t height, ColorChannel activeChannels = static_cast<ColorChannel>(15));
+			Img(const Img& image) = default;
+			Img(Img&& image) = default;
+
+			Img& operator=(const Img& image) = default;
+			Img& operator=(Img&& image) = default;
+
+			Pixel& operator()(uint64_t x, uint64_t y);
+			const Pixel& operator()(uint64_t x, uint64_t y) const;
+
+			Img subRect(uint64_t left, uint64_t top, uint64_t width, uint64_t height);
+
+			void setActiveChannels(ColorChannel activeChannels);
+			ColorChannel getActiveChannels() const;
+
+			~Img() = default;
+
+		private:
+
+			Mat _r;
+			Mat _g;
+			Mat _b;
+			Mat _a;
+
+			ColorChannel _activeChannels;
+	};
 }
--- include/DejaVu/types.hpp
+++ include/DejaVu/types.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <string>
+#include <cstdint>
+
 #include <SciPP/SciPP.hpp>
 
 namespace djv
 {
+	typedef scp::Mat<float> Mat;
+
 	class Img;
 }
