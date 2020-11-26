ced2aedb89dad3b035110edf6349a863cb78a3af
Reiex <reiex.marius38@gmail.com>
2020-11-26 20:03:50 +0100

    Ajout du detecteur de Marr Hildreth

--- examples/main.cpp
+++ examples/main.cpp
@@ -6,19 +6,27 @@ int main()
 
 	// Kernels
 
-	// djv::Img(scp::convolve(image.getComponent(djv::ColorComponent::R), djv::sobelKernel()[0])).saveToFile("build/sobelKernelX.png");
-	// djv::Img(scp::convolve(image.getComponent(djv::ColorComponent::R), djv::sobelKernel()[1])).saveToFile("build/sobelKernelY.png");
-	// djv::Img(scp::convolve(image.getComponent(djv::ColorComponent::R), djv::derivativeOfGaussianKernel()[0])*3.f).saveToFile("build/derivativeOfGaussianKernelX.png");
-	// djv::Img(scp::convolve(image.getComponent(djv::ColorComponent::R), djv::derivativeOfGaussianKernel()[1])*3.f).saveToFile("build/derivativeOfGaussianKernelY.png");
+	/*
+	djv::Img(scp::convolve(image.getComponent(djv::ColorComponent::R), djv::sobelKernel()[0])).saveToFile("build/sobelKernelX.png");
+	djv::Img(scp::convolve(image.getComponent(djv::ColorComponent::R), djv::sobelKernel()[1])).saveToFile("build/sobelKernelY.png");
+	djv::Img(scp::convolve(image.getComponent(djv::ColorComponent::R), djv::derivativeOfGaussianKernel()[0])*3.f).saveToFile("build/derivativeOfGaussianKernelX.png");
+	djv::Img(scp::convolve(image.getComponent(djv::ColorComponent::R), djv::derivativeOfGaussianKernel()[1])*3.f).saveToFile("build/derivativeOfGaussianKernelY.png");
 
-	// djv::Img(scp::convolve(image.getComponent(djv::ColorComponent::R), djv::laplacianOfGaussianKernel())*10.f).saveToFile("build/LoGKernel.png");
+	djv::Img(scp::convolve(image.getComponent(djv::ColorComponent::R), djv::laplacianOfGaussianKernel())*10.f).saveToFile("build/LoGKernel.png");
 
-	// djv::Img(scp::convolve(image.getComponent(djv::ColorComponent::R), djv::gaussianKernel(5.f))).saveToFile("build/gaussianKernel.png");
+	djv::Img(scp::convolve(image.getComponent(djv::ColorComponent::R), djv::gaussianKernel(5.f))).saveToFile("build/gaussianKernel.png");
+	*/
 
 	// Processings
 
+	/*
 	djv::Img(djv::gaussianBlur(image.getComponent(djv::ColorComponent::R), 5.f)).saveToFile("build/gaussianBlur.png");
 	djv::Img((djv::gaussianBlur(image.getComponent(djv::ColorComponent::R), 1.f) - djv::gaussianBlur(image.getComponent(djv::ColorComponent::R), 2.f))*10.f).saveToFile("build/DoG.png");
+	*/
+
+	// Edge detectors
+
+	djv::Img(djv::marrHildrethEdgeDetector(image.getComponent(djv::ColorComponent::R))).saveToFile("build/marrHildrethEdgeDetector.png");
 
 	return 0;
 }
--- include/DejaVu/ImageProcessing/EdgeDetection.hpp
+++ include/DejaVu/ImageProcessing/EdgeDetection.hpp
@@ -4,5 +4,5 @@
 
 namespace djv
 {
-	scp::Mat<float> marrHildrethEdgeDetector(scp::Mat<float> m, float sigma0, float scaleFactor);
+	scp::Mat<float> marrHildrethEdgeDetector(scp::Mat<float> m, float sigma0 = 1.f, float scaleFactor = 1.5f, float threshold = 0.001f);
 }
